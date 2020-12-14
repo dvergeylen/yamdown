fefa2bd7e2ab0e7f73d25617ee82a8c802dda170
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2020-12-14 16:13:47 +0100

    Initial commit ♥
    This includes neutralino binaries for now.
    This doesn't have my preference but I will stick with it for a proof-of-concept.
    I have successfully downloaded and built neutralinojs from source for linux / macos, but not for Windows (from Linux).
    I would prefer package neutralino compiled from source instead from github binaries but let's keep it simple for now.

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Information from './components/Information.svelte';
+    export let Neutralino;
+    export let versionInfo = {
+        NL_VERSION,
+        NL_PORT,
+        NL_OS,
+        NL_NAME
+    };
+</script>
+
+<style>
+    #neutralinoapp {
+    text-align: center;
+    }
+    #neutralinoapp h1{
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 20px;
+        color: #000000;
+    }
+</style>
+<div id="neutralinoapp">
+    <h1>NeutralinoJs</h1>
+    <Information versionInfo={versionInfo}></Information>
+</div>
